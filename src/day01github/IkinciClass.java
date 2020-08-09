10cd25be2fea267d3eb7e9b0b8697e7ce8adc55b
Ahmet <parisstar033@gmail.com>
2020-08-09 21:02:18 +0200

    ilk commitim

---
+++ src/day01github/IkinciClass.java
@@ -0,0 +1,10 @@
+package day01github;
+
+public class IkinciClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/day01github/IlkClass.java
@@ -0,0 +1,12 @@
+package day01github;
+
+public class IlkClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ahmet'ten selamlar...");
+		
+
+	}
+
+}
